34bbbb876e28cf66b297e52fbf85f60c95a035af
Caleb <caleb@DESKTOP-HKML8PJ.localdomain>
2020-10-22 22:50:42 -0400

    Rohan and Caleb fixed some bugs

--- simulators/filter2/src/PathSimulator.py
+++ simulators/filter2/src/PathSimulator.py
@@ -3,7 +3,15 @@ import numpy as np
 from WayPoint import WayPoint
 import conversions as conversions
 import csv
-
+import os
+# Bugs:
+# - under the trapazoid case, velocity and acceleration behave as desired. under triangle case, velocity does not 
+#   start slowing down soon enough such that it will end at zero by the time it goes far enough
+# - lat/long don't update in the csv
+# not implemented: 
+# - lat and long relative to the reference coordinantes in config/filter/filter[RefCoords]
+# - lat/long are still in degrees and minutes, need to make it just decimal degrees
+# - doesn't wait the time that's desired between segments
 
 
 class Simulator():
@@ -14,13 +22,13 @@ class Simulator():
 
     def __init__(self):
         self.waypoints = []
-        self.max_acceleration = 0.5
-        self.max_velocity = 2.0
-        self.dt = 0.1
-        self.threshold = 1e-3
+        self.max_acceleration: float = 0.5
+        self.max_velocity: float = 1.0
+        self.dt: float = 0.1
+        self.threshold: float = 1e-10
         # Hard coded file
 
-        with open("test1.csv", "r") as points:
+        with open(os.path.join(os.getcwd(),"test1.csv"), "r") as points:
             for line in points.readlines():
                 waypoint_info = line.split(",")
                 self.waypoints.append(WayPoint(float(waypoint_info[0]), float(waypoint_info[1]), float(waypoint_info[2])))
@@ -30,51 +38,57 @@ class Simulator():
         output = []
         for waypoint in self.waypoints[:-1]:
             next_waypoint = self.waypoints[self.waypoints.index(waypoint)+1]
-            distance_to_travel = self.distance(waypoint, next_waypoint) 
+            total_distance = self.distance(waypoint, next_waypoint) 
             bearing = self.calculate_bearing(waypoint, next_waypoint)
 
-            # Travel information
+            # Initialize Travel information
+            time_traveled = 0
+            distance_traveled = 0
+            distance_needed_to_slow = 0
+            distance_to_travel = total_distance
             velocity = 0
             acceleration = self.max_acceleration
-            time_traveled = self.dt
-            time_to_reach_max_velocity = self.max_velocity / self.max_acceleration
-            point = WayPoint(waypoint.north, waypoint.east, waypoint.time_to_sit)
-            distance_traveled = 0
-            
-            while distance_to_travel > self.threshold:
 
-                distance_needed_to_slow = (velocity)**2 / (2*acceleration)
-                if distance_to_travel <= distance_needed_to_slow:
+            while distance_to_travel >= self.threshold:
+
+                print("distance needed to slow:" + str(distance_needed_to_slow))
+                if distance_needed_to_slow >= distance_to_travel:
                     acceleration = -self.max_acceleration
-                    distance_traveled += velocity*self.dt + 0.5*acceleration*self.dt**2
-                    velocity -= acceleration * self.dt
+                    distance_traveled += velocity*self.dt + 0.5*acceleration*(self.dt)**2
+                    print("distance traveled:" + str(distance_traveled))
+                    velocity += acceleration * self.dt
                     north_change = math.sin(bearing) * distance_traveled
                     east_change = math.cos(bearing) * distance_traveled
-                    point.north += north_change
-                    point.east += east_change
-
+                    waypoint.north += north_change
+                    waypoint.east += east_change
+                    print("evaluated ayaserpoqiahfsd")
 
                 else:
-                    velocity = min(self.max_velocity, self.max_acceleration * time_traveled) 
-                    distance_traveled = 1/2*acceleration*(time_traveled)**2
+                    velocity = min(self.max_velocity, self.max_acceleration * time_traveled)
+                    if velocity == self.max_velocity:
+                        acceleration = 0 
+                    distance_traveled += velocity*self.dt + 0.5*acceleration*(self.dt)**2
                     north_change = math.sin(bearing) * distance_traveled
                     east_change = math.cos(bearing) * distance_traveled
-                    point.north += north_change
-                    point.east += east_change
+                    waypoint.north += north_change
+                    waypoint.east += east_change
+                    print("distance traveled: " + str(distance_traveled))
 
-                lat_info = self.calculate_lat(point)
-                long_info = self.calculate_long(point, lat_info[0])
+                lat_info = self.calculate_lat(waypoint)
+                long_info = self.calculate_long(waypoint, lat_info[0])
                 bearing_deg = self.calculate_bearing_deg(bearing)
                 output.append([lat_info[0], lat_info[1], long_info[0], long_info[1], velocity, acceleration, bearing_deg])
+
                 time_traveled += self.dt
-                distance_to_travel -= distance_traveled
+                distance_to_travel = total_distance - distance_traveled
+                distance_needed_to_slow = (velocity)**2 / (2*self.max_acceleration)
+                print("distance_to_travel:" + str(distance_to_travel))
         self.outputPoints(output)
     
 
-
     def distance(self, start: WayPoint, end: WayPoint):
         diff_north = math.fabs(end.north - start.north)
-        diff_east = math.fabs(end.east - start.north)
+        diff_east = math.fabs(end.east - start.east)
         return math.hypot(diff_north, diff_east)
 
     def calculate_lat(self, point: WayPoint):
@@ -146,6 +160,7 @@ class Simulator():
             Hard coded output file.
         '''
         f = open('test1out.csv', 'w')
+        f.write('lat_deg,lat_min,long_deg,long_min,velocity,acceleration,bearing_deg\n')
         for x in output:
             f.write(str(x[0]) + "," + str(x[1]) + "," + str(x[2]) + "," + str(x[3]) + "," + str(x[4]) + "," + \
                 str(x[5]) + "," + str(x[6]))
